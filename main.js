695b89c38cebccaa99196e591d473625ad0c0dd8
alvimkkj <148725513+alvimkkj@users.noreply.github.com>
2024-04-22 08:11:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,11 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-
+const tempoObjetivo1 = new Date("2024-12-05T00:10:00");
+const tempoObjetivo2 = new Date("2024-12-01T00:10:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:10:00");
+const tempoObjetivo4 = new Date("2024-10-01T00:10:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
@@ -51,8 +50,8 @@ function atualizaCronometro(){
 }
 
 function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
+    atualizaCronometro(0);
+    setInterval(atualizaCronometro,0);
 }
 
 comecaCronometro();
